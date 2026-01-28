4baceccaede2b604b83a248c712d5baff2452228
Tharith <tharith@example.com>
2026-01-28 14:35:38 +1100

    Add POST endpoint to EmployeeController for adding employees

--- initial/src/main/java/com/example/restservice/EmployeeController.java
+++ initial/src/main/java/com/example/restservice/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.example.restservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +16,11 @@ public class EmployeeController {
   @GetMapping("/employees")
   public Employees getEmployees() {
     return employeeManager.getEmployees();
+  }
+
+  @PostMapping("/employees")
+  public Employee addEmployee(@RequestBody Employee employee) {
+    employeeManager.addEmployee(employee);
+    return employee;
   }
 }
